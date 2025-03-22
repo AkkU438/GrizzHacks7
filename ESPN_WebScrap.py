25325f2e5801a26940772a256ce6cfec8f73121b
AkkU438 <109703098+AkkU438@users.noreply.github.com>
2025-03-22 13:16:39 -0400

    LLLSLS

---
+++ ESPN_WebScrap.py
@@ -0,0 +1,34 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+
+chrome_options = Options()
+chrome_options.add_argument("--headless")  # Run in the background
+service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service, options=chrome_options)
+
+
+url = 'https://www.nfldraftbuzz.com/positions/RB/1/2025'
+
+driver.get(url)
+driver.implicitly_wait(5)
+
+soup = BeautifulSoup(driver.page_source, 'html.parser')
+
+players = soup.find_all('h4', class_='team-meta_name')
+
+
+
+prospect_names = []
+for player in players:
+    first_name = player.find('span', class_='firstName').text.strip()
+    last_name = player.find('span', class_='lastName').text.strip()
+    print(first_name, last_name)
+    full_name = f"{first_name} {last_name}"
+    prospect_names.append(full_name)
+
+print(prospect_names)
